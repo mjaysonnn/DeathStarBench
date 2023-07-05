f7ac1d741b460cb79282e9267415cfe5f427f31a
Sourcery AI <>
2023-07-05 14:18:39 +0000

    'Refactored by Sourcery'

--- socialNetwork/gen-py/social_network/UserMentionService.py
+++ socialNetwork/gen-py/social_network/UserMentionService.py
@@ -79,15 +79,18 @@ class Client(Iface):
 class Processor(Iface, TProcessor):
     def __init__(self, handler):
         self._handler = handler
-        self._processMap = {}
-        self._processMap["ComposeUserMentions"] = Processor.process_ComposeUserMentions
+        self._processMap = {
+            "ComposeUserMentions": Processor.process_ComposeUserMentions
+        }
 
     def process(self, iprot, oprot):
         (name, type, seqid) = iprot.readMessageBegin()
         if name not in self._processMap:
             iprot.skip(TType.STRUCT)
             iprot.readMessageEnd()
-            x = TApplicationException(TApplicationException.UNKNOWN_METHOD, 'Unknown function %s' % (name))
+            x = TApplicationException(
+                TApplicationException.UNKNOWN_METHOD, f'Unknown function {name}'
+            )
             oprot.writeMessageBegin(name, TMessageType.EXCEPTION, seqid)
             x.write(oprot)
             oprot.writeMessageEnd()
@@ -214,7 +217,7 @@ class ComposeUserMentions_args(object):
     def __repr__(self):
         L = ['%s=%r' % (key, value)
              for key, value in self.__dict__.items()]
-        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+        return f"{self.__class__.__name__}({', '.join(L)})"
 
     def __eq__(self, other):
         return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
@@ -252,25 +255,19 @@ class ComposeUserMentions_result(object):
             (fname, ftype, fid) = iprot.readFieldBegin()
             if ftype == TType.STOP:
                 break
-            if fid == 0:
-                if ftype == TType.LIST:
-                    self.success = []
-                    (_etype285, _size282) = iprot.readListBegin()
-                    for _i286 in range(_size282):
-                        _elem287 = UserMention()
-                        _elem287.read(iprot)
-                        self.success.append(_elem287)
-                    iprot.readListEnd()
-                else:
-                    iprot.skip(ftype)
-            elif fid == 1:
-                if ftype == TType.STRUCT:
-                    self.se = ServiceException()
-                    self.se.read(iprot)
-                else:
-                    iprot.skip(ftype)
-            else:
+            if fid == 0 and ftype == TType.LIST:
+                self.success = []
+                (_etype285, _size282) = iprot.readListBegin()
+                for _i286 in range(_size282):
+                    _elem287 = UserMention()
+                    _elem287.read(iprot)
+                    self.success.append(_elem287)
+                iprot.readListEnd()
+            elif fid == 0 or fid == 1 and ftype != TType.STRUCT or fid != 1:
                 iprot.skip(ftype)
+            else:
+                self.se = ServiceException()
+                self.se.read(iprot)
             iprot.readFieldEnd()
         iprot.readStructEnd()
 
@@ -299,7 +296,7 @@ class ComposeUserMentions_result(object):
     def __repr__(self):
         L = ['%s=%r' % (key, value)
              for key, value in self.__dict__.items()]
-        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+        return f"{self.__class__.__name__}({', '.join(L)})"
 
     def __eq__(self, other):
         return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
