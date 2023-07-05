2d8310f4d07cf68d3a94f1e1472e471012f2655f
Sourcery AI <>
2023-07-05 14:17:56 +0000

    'Refactored by Sourcery'

--- socialNetwork/gen-py/social_network/MediaService.py
+++ socialNetwork/gen-py/social_network/MediaService.py
@@ -82,15 +82,16 @@ class Client(Iface):
 class Processor(Iface, TProcessor):
     def __init__(self, handler):
         self._handler = handler
-        self._processMap = {}
-        self._processMap["ComposeMedia"] = Processor.process_ComposeMedia
+        self._processMap = {"ComposeMedia": Processor.process_ComposeMedia}
 
     def process(self, iprot, oprot):
         (name, type, seqid) = iprot.readMessageBegin()
         if name not in self._processMap:
             iprot.skip(TType.STRUCT)
             iprot.readMessageEnd()
-            x = TApplicationException(TApplicationException.UNKNOWN_METHOD, 'Unknown function %s' % (name))
+            x = TApplicationException(
+                TApplicationException.UNKNOWN_METHOD, f'Unknown function {name}'
+            )
             oprot.writeMessageBegin(name, TMessageType.EXCEPTION, seqid)
             x.write(oprot)
             oprot.writeMessageEnd()
@@ -236,7 +237,7 @@ class ComposeMedia_args(object):
     def __repr__(self):
         L = ['%s=%r' % (key, value)
              for key, value in self.__dict__.items()]
-        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+        return f"{self.__class__.__name__}({', '.join(L)})"
 
     def __eq__(self, other):
         return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
@@ -275,20 +276,14 @@ class ComposeMedia_result(object):
             (fname, ftype, fid) = iprot.readFieldBegin()
             if ftype == TType.STOP:
                 break
-            if fid == 0:
-                if ftype == TType.STRUCT:
-                    self.success = Media()
-                    self.success.read(iprot)
-                else:
-                    iprot.skip(ftype)
-            elif fid == 1:
-                if ftype == TType.STRUCT:
-                    self.se = ServiceException()
-                    self.se.read(iprot)
-                else:
-                    iprot.skip(ftype)
-            else:
+            if fid == 0 and ftype == TType.STRUCT:
+                self.success = Media()
+                self.success.read(iprot)
+            elif fid == 0 or fid == 1 and ftype != TType.STRUCT or fid != 1:
                 iprot.skip(ftype)
+            else:
+                self.se = ServiceException()
+                self.se.read(iprot)
             iprot.readFieldEnd()
         iprot.readStructEnd()
 
@@ -314,7 +309,7 @@ class ComposeMedia_result(object):
     def __repr__(self):
         L = ['%s=%r' % (key, value)
              for key, value in self.__dict__.items()]
-        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+        return f"{self.__class__.__name__}({', '.join(L)})"
 
     def __eq__(self, other):
         return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
